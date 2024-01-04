1d8bc8e88e0887994ce950e64312607d102353f2
Berker ERTAN <berkerertan@hotmail.com>
2024-01-05 00:38:02 +0300

    9.gün DTO eklendi

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -8,12 +8,28 @@ internal class Program
     //Open Closed Principle
     private static void Main(string[] args)
     {
-        ProductManager productManager = new ProductManager(new EfProductDal());
-        foreach (var product in productManager.GetByUnitPrice(50,100))
+        ProductTest();
+        //IoC
+        //CategoryTest();
+
+    }
+
+    private static void CategoryTest()
+    {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        foreach (var category in categoryManager.GetAll())
         {
-            Console.WriteLine(product.ProductName);
+            Console.WriteLine(category.CategoryName);
+        }
+    }
+
+    private static void ProductTest()
+    {
+        ProductManager productManager = new ProductManager(new EfProductDal());
+        foreach (var product in productManager.GetProductDetails())
+        {
+            Console.WriteLine(product.ProductName + "/" + product.CategoryName);
 
         }
-        
     }
 }
