2330e2f5c6e6edfbb9c22d2dd86254ee4ca8e858
Berker ERTAN <berkerertan@hotmail.com>
2024-01-04 03:41:47 +0300

    9.Gün Core Eklendi

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,5 +1,5 @@
-﻿using Entities.Abstract;
-using Entities.Concrete;
+﻿
+using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,13 +7,13 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
     //generic constraint
     //class: referans tip
     // IEntity : IEntity veya IEntityden türetilmiş olmalı
     //new() : new'lenebilir olmalı
-    public interface IEntityRepository<T> where T : class,IEntity,new()
+    public interface IEntityRepository<T> where T : class, IEntity, new()
     {
         List<T> GetAll(Expression<Func<T, bool>> filter = null);
         T Get(Expression<Func<T, bool>> filter);
